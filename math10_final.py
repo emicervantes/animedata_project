0a904359b42ac3b3f7c10c4179d0197b2092f40b
emicervantes <91242164+emicervantes@users.noreply.github.com>
2021-12-06 23:52:43 -0800

    Update math10_final.py

--- math10_final.py
+++ math10_final.py
@@ -128,6 +128,7 @@ st.sidebar.write()
 st.sidebar.image(image, width = 250)
 st.sidebar.write("My Github! [Click here!](https://github.com/emicervantes)")
 st.sidebar.write("Dataset Link: [dataanime.csv](https://www.kaggle.com/canggih/anime-data-score-staff-synopsis-and-genre)")
+st.sidebar.write("Anime Recommendation Source Link: [YouTube](https://www.youtube.com/watch?v=XoTwndOgXBM&t=4824s)")
 st.sidebar.write()
 st.sidebar.write("Hello! My name is emi, and I am a huge anime fan! My favorite anime is Jujutsu Kaisen. The big question of this app is to find if the time each anime was aired determined its genre. I created a model using Scikit-learn LogisticRegression algorithm to predict the genre of anime based on its score and the time it was aired. Another feature that I added was the anime recommendation engine using Scikit-learn with a concept of cosine similarity.")
 
